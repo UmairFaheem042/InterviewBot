9ac9be494d7533caaa71ceae2c8eb2687a286a18
Umair <umair.freelancer042@gmail.com>
2024-11-11 19:58:31 +0530

    On deployed website img was not visible so changed to Image and download the image asset

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -8,8 +8,9 @@ export default function Page() {
         <aside className="relative sm:block hidden lg:order-last lg:col-span-5 lg:h-full xl:col-span-6  ">
           <Image
             alt="side image"
-            src="https://images.unsplash.com/photo-1516962215378-7fa2e137ae93?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="/authImage.jpg"
             className="absolute inset-0 h-full w-full object-cover"
+            fill
           />
         </aside>
 
